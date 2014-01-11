83bdc3ae813eaf6811015ae6ab9fb16402274bee
Joze Carlos <jozecarlos.it@gmail.com>
2014-01-11 15:39:27 -0200

    security user repository

--- src/Izze/UserBundle/DataFixtures/ORM/UserFixtures.php
+++ src/Izze/UserBundle/DataFixtures/ORM/UserFixtures.php
@@ -1,36 +1,52 @@
 <?php
 // src/Blogger/BlogBundle/DataFixtures/ORM/BlogFixtures.php
 
-namespace Blogger\BlogBundle\DataFixtures\ORM;
+namespace Izze\UserBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Izze\UserBundle\Entity\User;
 use Izze\UserBundle\Entity\Role;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class BlogFixtures implements FixtureInterface
+class UserFixtures implements FixtureInterface, ContainerAwareInterface
 {
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     public function load(ObjectManager $manager)
     {
 
-         $role = new Role();
-         $role->setName("COMMON USER");
-         $role->setRole("ROLE_USER");
-         $manager->persist($role);
+        $role = new Role();
+        $role->setName("COMMON USER");
+        $role->setRole("ROLE_USER");
+        $manager->persist($role);
 
-        $factory = $this->get('security.encoder_factory');
         $user = new User();
-        $encoder = $factory->getEncoder($user);
+
+        $encoder = $this->container
+            ->get('security.encoder_factory')
+            ->getEncoder($user)
+        ;
 
         $user->setUsername("jozecarlos");
         $user->setPassword($encoder->encodePassword('123456', $user->getSalt()));
         $user->setEmail("o.grande.ze@gmail.com");
-        $user->setRoles($user->getRoles());
+        $user->addRole($role);
 
         $manager->persist($user);
-
-        
-
         $manager->flush();
     }
 
